8769cdca806133252a631ae95781d0d32938b52b
Lucas <krawdev@gmail.com>
2024-08-07 17:10:08 -0300

    feat: project created

---
+++ src/main/java/com/luckraw/eventos_tec/EventosTecApplication.java
@@ -0,0 +1,13 @@
+package com.luckraw.eventos_tec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventosTecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventosTecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luckraw/eventos_tec/EventosTecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luckraw.eventos_tec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosTecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
